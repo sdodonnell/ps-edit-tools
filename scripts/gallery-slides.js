2e915f79ccec0aba79f3603c6992ffc1f2087122
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-05-26 13:49:44 -0700

    Add gallery slide toolbars (#3)

---
+++ scripts/gallery-slides.js
@@ -0,0 +1,114 @@
+/**
+ * Code to insert tooltip icons next to gallery slides
+ **/
+(function() {
+    const gallerySlideContainers = document.querySelectorAll('div[data-element-type="ParagraphSlide"]');
+    let isShowing = false;
+
+    const addIndexToToolbar = (index, toolbar) => {
+        const number = document.createElement('div');
+        number.innerText = index + 1;
+        toolbar.appendChild(number);
+    }
+
+    const getImageEditUrl = (nid, imageNid, index) => {
+        const pageNum = Math.ceil((index + 1) / 25);
+        return `https://www.popsugar.com/node/${nid}/edit/photos?page=${pageNum}#${imageNid}`
+    }
+
+    const getImageSizesUrl = (imageNid) => {
+        return `https://www.popsugar.com/image/sizes/${imageNid}`
+    }
+
+    const addEditImageUrlToToolbar = (slide, index, toolbar) => {
+        const toolbarItem = document.createElement('a');
+        toolbarItem.title = 'Edit Image';
+        toolbarItem.target = "_blank";
+
+        const imageNid = slide.id.split('-')[1];
+        toolbarItem.href = getImageEditUrl(nid, imageNid, index);
+
+        const toolbarItemImage = document.createElement('img');
+        toolbarItemImage.src = chrome.runtime.getURL('images/camera.png');
+
+        toolbarItem.appendChild(toolbarItemImage);
+        toolbar.insertBefore(toolbarItem, toolbar.childNodes[2]);
+    }
+
+    const addImageSizesUrlToToolbar = (slide, toolbar) => {
+        const toolbarItem = document.createElement('a');
+        toolbarItem.title = 'Image Sizes';
+        toolbarItem.target = "_blank";
+
+        const imageNid = slide.id.split('-')[1];
+        toolbarItem.href = getImageSizesUrl(imageNid);
+
+        const toolbarItemImage = document.createElement('img');
+        toolbarItemImage.src = chrome.runtime.getURL('images/photo.png');
+
+        toolbarItem.appendChild(toolbarItemImage);
+        toolbar.insertBefore(toolbarItem, toolbar.childNodes[3]);
+    }
+    
+    if (gallerySlideContainers.length > 0 && window.innerWidth > 768) {
+        gallerySlideContainers.forEach((slide, index) => {
+            const toolbarContainer = document.createElement('div');
+            toolbarContainer.classList.add('ps-gallery-slide-toolbar', 'hide');
+            const toolbar = document.createElement('div');
+            toolbarContainer.appendChild(toolbar);
+    
+            // Add links to toolbar
+            const toolbarItems = {
+                //
+                // The first item will always be the index.
+                //
+                editPost: {
+                    show: true,
+                    getLink: getEditUrl,
+                    image: 'images/edit.png',
+                    title: 'Edit Post'
+                },
+                //
+                // The third item will be the image edit URL, which is generated below.
+                //
+                //
+                // The fourth item will be the edit image sizes URL, which is generated below.
+                //
+                addPhotos: {
+                    show: true,
+                    getLink: getAddPhotosUrl,
+                    image: 'images/plus.png',
+                    title: 'Add Photos'
+                }
+            }
+    
+            addIndexToToolbar(index, toolbar);
+            addLinksToToolbar(toolbarItems, toolbar);
+            addEditImageUrlToToolbar(slide, index, toolbar);
+            addImageSizesUrlToToolbar(slide, toolbar);
+    
+            slide.insertBefore(toolbarContainer, slide.firstChild);
+        })
+    
+        const toolbars = document.querySelectorAll('.ps-gallery-slide-toolbar')
+    
+        const hideGalleryToolbars = () => {
+            toolbars.forEach(toolbar => toolbar.classList.add('hide'));
+            isShowing = false;
+        }
+    
+        const showGalleryToolbars = () => {
+            toolbars.forEach(toolbar => toolbar.classList.remove('hide'));
+            isShowing = true;
+        }
+    
+        window.addEventListener('keydown', e => {
+            if (e.key === '?' && !isShowing) {
+                showGalleryToolbars();
+            } else if (e.key === '?' && isShowing) {
+                hideGalleryToolbars();
+            }
+        })
+    }
+
+})()
--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -1,182 +1,91 @@
 /**
  * Code to insert tooltip icons above the header of PS nodes
  **/
-
-const header = document.querySelector('.article-header');
-let isShowing = false;
-
-
-/**
- * Link getters
- */
-
-// Functions to generate helpful links based on current page.
-// These will either return a string or a function. A string will be
-// a URL; a function will send a message to the extension service
-// worker that will open a new window.
-const getAppleNewsUrl = () => {
-    const nextData = JSON.parse(document.getElementById('__NEXT_DATA__')?.innerText || '{}');
-    const appleNewsId = nextData?.props?.pageProps?.page?.apple_news_id;
-
-    if (appleNewsId) {
-        const apiUrl = `https://popsugar.com/api/news/getarticle?uid=${appleNewsId}`;
-
-        return async () => {
-            const apiResponse = await fetch(apiUrl);
-            const apiResponseJson = await apiResponse.json();
-            const url = apiResponseJson.url;
-            const data = {
-                type: 'appleNews',
-                url
-            }
+(function() {
+    const header = document.querySelector('.article-header');
+    let isShowing = false;
     
-            await chrome.runtime.sendMessage(data);
+    // Don't add toolbar on mobile, or on non-node pages
+    if (header && window.innerWidth > 768) {
+        const toolbarItems = {
+            appleNews: {
+                show: true,
+                getLink: getAppleNewsUrl,
+                image: 'images/apple-news.png',
+                title: 'Apple News Link'
+            },
+            amp: {
+                show: true,
+                getLink: getAmpUrl,
+                image: 'images/flash.png',
+                title: 'AMP preview'
+            },
+            mobile: {
+                show: true,
+                getLink: getMobileUrl,
+                image: 'images/iphone.png',
+                title: 'Mobile preview'
+            },
+            analytics: {
+                show: true,
+                getLink: getAnalyticsUrl,
+                image: 'images/analytics.png',
+                title: 'Analytics dashboard'
+            },
+            timers: {
+                show: true,
+                getLink: getTimersUrl,
+                image: 'images/clock.png',
+                title: 'View timers'
+            },
+            edit: {
+                show: true,
+                getLink: getEditUrl,
+                image: 'images/edit.png',
+                title: 'Edit post'
+            },
         }
-    }
-
-    return null;
-}
-
-const getAmpUrl = (_, path, host) => {
-    return `${host}/amphtml/${path}`
-}
-
-const getMobileUrl = () => {
-    const url = window.location.href
-    return async () => {
-        const data = {
-            type: 'mobile',
-            url
+    
+        // Create toolbar DOM node
+        const toolbarContainer = document.createElement('div');
+        toolbarContainer.classList.add('ps-edit-toolbar', 'hide');
+        const toolbar = document.createElement('div');
+        toolbarContainer.appendChild(toolbar);
+    
+        addLinksToToolbar(toolbarItems, toolbar)
+    
+        header.insertBefore(toolbarContainer, header.firstChild);
+    
+        const hideToolbar = () => {
+            toolbarContainer.classList.add('hide');
+            isShowing = false;
         }
-
-        await chrome.runtime.sendMessage(data);
-    }
-}
-
-const getAnalyticsUrl = nid => {
-    return `https://popsugar.com/dashboard/analytics/overview?nid=${nid}`
-}
-
-const getTimersUrl = nid => {
-    return `https://popsugar.com/${nid}/edit?#publishing_options`
-}
-
-const getEditUrl = nid => {
-    return `https://popsugar.com/${nid}/edit`
-}
-
-/**
- * Helpers
- */
-
-const isLocal = host => host.includes('dev.popsugar.com');
-const isStaging = host => host.includes('.p3.staging.popsugar.com');
-
-// Don't add toolbar on mobile, or on non-node pages
-if (header && window.innerWidth > 768) {
-    const toolbarItems = {
-        appleNews: {
-            show: true,
-            getLink: getAppleNewsUrl,
-            image: 'images/apple-news.png',
-            title: 'Apple News Link'
-        },
-        amp: {
-            show: true,
-            getLink: getAmpUrl,
-            image: 'images/flash.png',
-            title: 'AMP preview'
-        },
-        mobile: {
-            show: true,
-            getLink: getMobileUrl,
-            image: 'images/iphone.png',
-            title: 'Mobile preview'
-        },
-        analytics: {
-            show: true,
-            getLink: getAnalyticsUrl,
-            image: 'images/analytics.png',
-            title: 'Analytics dashboard'
-        },
-        timers: {
-            show: true,
-            getLink: getTimersUrl,
-            image: 'images/clock.png',
-            title: 'View timers'
-        },
-        edit: {
-            show: true,
-            getLink: getEditUrl,
-            image: 'images/edit.png',
-            title: 'Edit post'
-        },
-    }
-
-    // Get nid from current page
-    const nid = window.location.pathname.split('-').reverse()[0];
-    const path = window.location.pathname;
-    const host = window.location.origin;
-
-    // Create toolbar DOM node
-    const toolbarContainer = document.createElement('div');
-    toolbarContainer.classList.add('ps-edit-toolbar', 'hide');
-    const toolbar = document.createElement('div');
-    toolbarContainer.appendChild(toolbar);
-
-    // Generate toolbar items and add to parent DOM node
-    Object.values(toolbarItems).forEach(item => {
-        if (item.show) {
-            const toolbarItem = document.createElement('a');
-            toolbarItem.title = item.title;
-            toolbarItem.target = "_blank";
-
-            const getter = item.getLink(nid, path, host);
-            if (typeof getter === 'function') {
-                toolbarItem.onclick = getter;
-            } else {
-                toolbarItem.href = getter;
+    
+        const showToolbar = () => {
+            toolbarContainer.classList.remove('hide');
+            isShowing = true;
+        }
+    
+        const addEventListeners = () => {
+            toolbarContainer.addEventListener("pointerenter", showToolbar);
+            toolbarContainer.addEventListener("pointerleave", hideToolbar);
+        }
+    
+        const removeEventListeners = () => {
+            toolbarContainer.removeEventListener('pointerleave', hideToolbar);
+            toolbarContainer.removeEventListener('pointerenter', showToolbar);
+        }
+    
+        window.addEventListener('keydown', e => {
+            if (e.key === '?' && !isShowing) {
+                showToolbar();
+                removeEventListeners();
+            } else if (e.key === '?' && isShowing) {
+                hideToolbar();
+                addEventListeners();
             }
-
-            const toolbarItemImage = document.createElement('img');
-            toolbarItemImage.src = chrome.runtime.getURL(item.image);
-
-            toolbarItem.appendChild(toolbarItemImage);
-            toolbar.appendChild(toolbarItem);
-        }
-    })
-
-    header.insertBefore(toolbarContainer, header.firstChild);
-
-    const hideToolbar = () => {
-        toolbarContainer.classList.add('hide');
-        isShowing = false;
+        })
+    
+        addEventListeners();
     }
-
-    const showToolbar = () => {
-        toolbarContainer.classList.remove('hide');
-        isShowing = true;
-    }
-
-    const addEventListeners = () => {
-        toolbarContainer.addEventListener("pointerenter", showToolbar);
-        toolbarContainer.addEventListener("pointerleave", hideToolbar);
-    }
-
-    const removeEventListeners = () => {
-        toolbarContainer.removeEventListener('pointerleave', hideToolbar);
-        toolbarContainer.removeEventListener('pointerenter', showToolbar);
-    }
-
-    window.addEventListener('keydown', e => {
-        if (e.key === '?' && !isShowing) {
-            showToolbar();
-            removeEventListeners();
-        } else if (e.key === '?' && isShowing) {
-            hideToolbar();
-            addEventListeners();
-        }
-    })
-
-    addEventListeners();
-}
+})()
